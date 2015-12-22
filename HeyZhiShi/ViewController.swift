055cd7cc29c1a2d414629959cc6085bafcdbc796
Demo861025 <Demo@huhaiqingdemacbook-pro.local>
2015-12-22 13:31:49 +0800

    Initial Commit

---
+++ HeyZhiShi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeyZhiShi
+//
+//  Created by 胡海清 on 15/12/22.
+//  Copyright © 2015年 胡海清. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
